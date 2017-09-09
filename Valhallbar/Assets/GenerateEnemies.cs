76c584afd959419a45c9a6137fbdbf3d1bd94c9b
Ruben Weituschewitz <weituschewitz@gmail.com>
2017-09-09 15:24:00 +0200

    Added dev sprites, ruler and first scripts.

---
+++ Valhallbar/Assets/GenerateEnemies.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemies : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+
+    // Use this for initialization
+	void Start () {
+	    var input = (TextAsset) Resources.Load("input");
+	    var lines = input.text.Split(new [] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+	    foreach (var line in lines)
+	    {
+	        var strings = line.Split(',');
+	        var time = int.Parse(strings[0]);
+	        var lane = int.Parse(strings[1]);
+	        var enemyType = int.Parse(strings[2]);
+
+	        var laneOffset = 2;
+            
+            var newObj = Instantiate(EnemyPrefab);
+            newObj.transform.SetParent(transform);
+	        newObj.transform.position = new Vector3(laneOffset * lane, -time/1000*2);
+	    }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Valhallbar/Assets/MoveUpwards.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MoveUpwards : MonoBehaviour
+{
+    public float Speed = 2;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    this.transform.Translate(0, Speed * Time.deltaTime, 0);
+	}
+}
