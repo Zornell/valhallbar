2d6ac21b74df4a60fc2f15edd9f0c80329a2e684
Ruben Weituschewitz <weituschewitz@gmail.com>
2017-09-10 00:10:18 +0200

    Added lights.

---
+++ Valhallbar/Assets/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private float _freq;
+    public float Direction = 1;
+
+    // Use this for initialization
+	void Start ()
+	{
+	    _freq = Random.Range(50, 200) * Direction;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		transform.Rotate(Vector3.forward, Time.deltaTime * _freq);
+	}
+}
