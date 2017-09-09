5fdb908edd09fcc30711556b694d6ba4a5e84827
Lorenz Braun <lorenz.braun@ziti.uni-heidelberg.de>
2017-09-09 16:47:38 +0200

    added Viking Object with movement

---
+++ Valhallbar/Assets/Viking.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Viking : MonoBehaviour {
+
+	int lane = 3;
+	int laneOffset = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Input.anyKeyDown);
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow) && lane > 1) {
+			lane -= 1;
+			Debug.Log ("lane++");
+		} else if (Input.GetKeyDown (KeyCode.RightArrow) && lane < 5) {
+			lane += 1;
+			Debug.Log ("lane--");
+		}
+
+		this.transform.position = new Vector3(- 3*laneOffset + lane*laneOffset, 4, 0);
+	}
+}
