c048152391876804718b3abcb5c28aaa8029e38c
Ruben Weituschewitz <weituschewitz@gmail.com>
2017-09-10 17:24:10 +0200

    Added booing.

--- Valhallbar/Assets/Scripts/SceneJainitor.cs
+++ Valhallbar/Assets/Scripts/SceneJainitor.cs
@@ -1,15 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SceneJainitor : MonoBehaviour {
 
 	private bool menuLoaded = false;
+    private AudioSource _source;
 
 	// Use this for initialization
 	void Start () {
         SceneManager.sceneLoaded += SceneManagerOnSceneLoaded;
 		SceneManager.LoadScene ("stage",LoadSceneMode.Additive);
+	    _source = GetComponent<AudioSource>();
 	}
 
     private void SceneManagerOnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
@@ -18,17 +21,29 @@ public class SceneJainitor : MonoBehaviour {
         {
             var component = GameObject.Find("Start").GetComponent<Button>();
             component.onClick.AddListener(StartGame);
+
+            var scrollText = GameObject.Find("ScrollText").GetComponent<Text>();
+            scrollText.CrossFadeAlpha(0.0f, 2.0f, true);
         }
     }
 
     private void StartGame()
     {
+        StartCoroutine(StartWithGame());
+        _source.Play();
+        var mainMenu = GameObject.Find("MainMenu");
+        mainMenu.SetActive(false);
+    }
+
+    private IEnumerator StartWithGame()
+    {
+        yield return new WaitForSeconds(6f);
         SceneManager.LoadScene("tavern", LoadSceneMode.Single);
     }
 
     // Update is called once per frame
 	void Update () {
-		if (Time.time > 10 && Input.anyKeyDown && !menuLoaded ) {
+		if ((Input.anyKeyDown || Time.time>30f) && !menuLoaded ) {
 			SceneManager.LoadScene ("mainmenu", LoadSceneMode.Additive);
 			menuLoaded = true;
 			Debug.Log ("loaded Mainmenu.");
