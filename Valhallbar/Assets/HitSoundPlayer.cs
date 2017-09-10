908055259b80237f4791508f342ca3fe2a5e4710
Ruben Weituschewitz <weituschewitz@gmail.com>
2017-09-10 12:53:07 +0200

    Sounds

---
+++ Valhallbar/Assets/HitSoundPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(GameManager))]
+[RequireComponent(typeof(AudioSource))]
+public class HitSoundPlayer : MonoBehaviour
+{
+    public AudioSource HitSoundSource;
+    public AudioClip[] HitSounds;
+    private GameManager _gameManager;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        _gameManager = GetComponent<GameManager>();
+        _gameManager.EnemyKilled += GameManagerOnEnemyKilled;
+    }
+
+    private void GameManagerOnEnemyKilled(object sender, EventArgs eventArgs)
+    {
+        var soundIdx = Mathf.FloorToInt(UnityEngine.Random.Range(0, HitSounds.Length - 0.1f));
+        HitSoundSource.PlayOneShot(HitSounds[soundIdx]);
+    }
+}
